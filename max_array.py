09bd54272ae3752867062cda9893643c0f8e0264
moontripathi <33838297+moontripathi@users.noreply.github.com>
2022-02-13 11:57:05 +0530

    Add files via upload

---
+++ max_array.py
@@ -0,0 +1,31 @@
+def maxsub(nums):
+        add = [None] * len(nums)
+        flag=0
+        new=[]
+        add[0]=nums[0]
+        #for creating the subarray of additions
+        for i in range(1,len(nums)):
+            check = add[i-1]+nums[i]
+            if check > 0 and check > nums[i]:
+                add[i]=check
+            else:
+                add[i]=nums[i]
+        start = add.index(max(add))
+        #finding out the max subarray in list
+        for i in range(start,-1,-1):
+            if add[i] >=0 :
+                flag =flag+1
+            else:
+                break
+        #printing the value
+        while(flag!=0):
+            new.append(nums[start])
+            start=start-1
+            flag=flag-1
+        add.clear()
+        new.reverse()
+        print(new)
+            
+l=[-2,1,-3,4,-1,2,1,-5,4]
+maxsub(l)
+
